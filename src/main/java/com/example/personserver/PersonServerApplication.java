fea56e6ae84db735f09685fcc69744bdb23339f0
Yuriy Fomin <yuriy.fomin@coro.net>
2023-07-22 18:10:37 +0100

    Initial commit

---
+++ src/main/java/com/example/personserver/PersonServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.personserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/personserver/security/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.personserver.security;public class SecurityConfig {
+}
